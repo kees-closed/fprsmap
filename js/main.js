c073d4fbdf134eda2c31ff16fad8afa51ee28f74
K. de Jong <kjong+dev@neobits.nl>
2022-09-07 08:23:39 +0200

    Use local chapter data instead of from the Internet

--- js/main.js
+++ js/main.js
@@ -115,7 +115,7 @@
 
   function initMap(initialMapCenter, initialZoomLevel, defaultOverlays) {
     var baseLayer = L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a> contributors, &copy; <a href="https://carto.com/attributions" target="_blank">CARTO</a> | &copy; <a href="https://github.com/WeAreFairphone/fprsmap" target="_blank">WeAreFairphone</a> (<a href="https://www.gnu.org/licenses/gpl-3.0.en.html" target="_blank">GPLv3</a>) | <a href="https://github.com/kees-closed/fprsmap" target="_blank">TZM</a> (<a href="https://www.gnu.org/licenses/gpl-3.0.en.html" target="_blank">GPLv3</a>)',
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a> contributors, &copy; <a href="https://carto.com/attributions" target="_blank">CARTO</a> | &copy; <a href="https://github.com/WeAreFairphone/fprsmap" target="_blank">WeAreFairphone</a> (<a href="https://www.gnu.org/licenses/gpl-3.0.en.html" target="_blank">GPLv3</a>) | &copy; <a href="https://github.com/kees-closed/fprsmap" target="_blank">TZM</a> (<a href="https://www.gnu.org/licenses/gpl-3.0.en.html" target="_blank">GPLv3</a>)',
       maxZoom: 18,
     });
 
@@ -304,7 +304,7 @@
   map.on('zoomend', onZoomend);
   map.on('moveend', onMoveend);
 
-  fetchJSON(GROUP_URL + '.json?filter=chapter')
+  fetchJSON('current_chapters.json')
     .then(function(json) {
       json.groups.forEach(function(chapter) {
         if (chapter.custom_fields.show_map && chapter.custom_fields.lat && chapter.custom_fields.lon) {
