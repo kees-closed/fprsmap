be828d5f6c3fdb3ea623d489d98219ac465dad57
K. de Jong <kjong+dev@neobits.nl>
2022-09-08 19:40:46 +0200

    Cleaned up the attribution, should not be a paragraph

--- js/main.js
+++ js/main.js
@@ -102,7 +102,6 @@
 
   function initMap(initialMapCenter, initialZoomLevel, defaultOverlays) {
     var baseLayer = L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a> contributors, &copy; <a href="https://carto.com/attributions" target="_blank">CARTO</a> | &copy; <a href="https://github.com/WeAreFairphone/fprsmap" target="_blank">WeAreFairphone</a> (<a href="https://www.gnu.org/licenses/gpl-3.0.en.html" target="_blank">GPLv3</a>) | &copy; <a href="https://github.com/kees-closed/fprsmap" target="_blank">TZM</a> (<a href="https://www.gnu.org/licenses/gpl-3.0.en.html" target="_blank">GPLv3</a>)',
       maxZoom: 18,
     });
 
@@ -138,9 +137,6 @@
 
   function updateEmbedTextareaContentAndBrowserUrl() {
     var queryParams = '?center=' + currentMapCenter[0] + ',' + currentMapCenter[1] + '&zoom=' + currentZoomLevel + '&show=' + activeLayers.toString();
-    embedTextareaContent = '<iframe src="https://map.tzm.community/' + queryParams + '" width="100%" height="400" allowfullscreen="true" frameborder="0">\n' +
-    '<p><a href="https://map.tzm.community/' + queryParams + '" target="_blank">See The Global Chapters Map!</a></p>\n' +
-    '</iframe>';
     try {
     if (window.history.replaceState) {
         window.history.replaceState({}, null, queryParams);
@@ -283,6 +279,8 @@
   map.on('overlayremove', onOverlayremove);
   map.on('zoomend', onZoomend);
   map.on('moveend', onMoveend);
+
+  map.attributionControl.setPrefix('<a href="https://forum.tzm.community/powered-by" title="This map was possible by the open-source community"> target="_blank">Powered by...</a>');
 
   fetchJSON('current_chapters.json')
     .then(function(json) {
